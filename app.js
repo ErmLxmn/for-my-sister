873f0b62fef38e25f8da682236748efcd1d5c21e
Ermel Laxamana <Ermel.Laxamana@filamsoftware.com>
2022-06-18 01:03:10 +0800

    words enhancement

--- app.js
+++ app.js
@@ -20,15 +20,25 @@ app.get('/', function (req, res) {
 app.get('/options', function (req, res) {
   let display = {
     index: 'index',
-    select1: true,
-    select2: false,
+    numbers: true,
+    words: false,
+    words2: false,
     display: 'numbers',
   }
 
   if (req.query.display === 'words') {
     display.index = 'index2'
-    display.select1 = false
-    display.select2 = true
+    display.numbers = false
+    display.words = true
+    display.words2 = false
+    display.display = 'words'
+  }
+
+  if (req.query.display === 'words2') {
+    display.index = 'index3'
+    display.numbers = false
+    display.words = false
+    display.words2 = true
     display.display = 'words'
   }
 
@@ -37,31 +47,47 @@ app.get('/options', function (req, res) {
 
 app.post('/api/answer1', function (req, res) {
   let answer = obj.solverMain(req.body.problem)
-  res.render('index', { answer1: answer, display: 'numbers', select1: true })
+  res.render('index', { answer1: answer, display: 'numbers', numbers: true })
 })
 
 app.post('/api/answer2', function (req, res) {
-  let word1 = req.body.problem1.split(' ')
-  let word2 = req.body.problem2.split(' ')
+  let word =
+    req.body.problem1 + '\r\n' + req.body.operator + '\r\n' + req.body.problem2
 
-  word1 = word1.map(function (w) {
-    return 'fourthy' === w ? 'forty' : w && 'fifthy' === w ? 'fifty' : w
-  })
-
-  word2 = word2.map(function (w) {
-    return 'fourthy' === w ? 'forty' : w && 'fifthy' === w ? 'fifty' : w
-  })
-
-  let problem = []
-  word1 = word1.join(' ')
-  word2 = word2.join(' ')
-  problem.push(wordsToNumbers(word1))
-  problem.push(req.body.operator)
-  problem.push(wordsToNumbers(word2))
-
+  let problem = word
+    .trim()
+    .split('\r\n')
+    .map(function (w) {
+      w = w.replace('fourthy', 'forty')
+      w = w.replace('fifthy', 'fifty')
+      return w
+    })
+    .map((value) => {
+      return wordsToNumbers(value)
+    })
   let newProblem = problem.join(' ')
   let answer = obj.solverMain(newProblem)
-  res.render('index2', { answer2: answer, display: 'words', select2: true })
+  res.render('index2', { answer2: answer, display: 'words', words: true })
+})
+
+app.post('/api/answer3', function (req, res) {
+  let newArr = req.body.fulltext
+    .trim()
+    .split('\r\n')
+    .filter((value) => {
+      return value !== 'Cashyshop - Buy load on Messenger'
+    })
+    .map((w) => {
+      w = w.replace('fourthy', 'forty')
+      w = w.replace('fifthy', 'fifty')
+      return w
+    })
+    .map((value) => {
+      return wordsToNumbers(value)
+    })
+  let newProblem = newArr.join(' ')
+  let answer = obj.solverMain(newProblem)
+  res.render('index3', { answer3: answer, display: 'words2', words2: true })
 })
 
 app.listen(process.env.PORT || 5000)
